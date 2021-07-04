620f79991285fdccc08ebf0b7412e73b97e70c5d
samir22072 <sami22072@gmail.com>
2021-07-04 17:01:50 +0530

    PBL project

---
+++ users/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms.fields import EmailField
+from .models import profile
+class UserRegisterForm(UserCreationForm):
+    email=EmailField()
+
+    class Meta:
+        model=User
+        fields=["username","email","password1","password2"]
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = profile
+        fields = ['image']
+
